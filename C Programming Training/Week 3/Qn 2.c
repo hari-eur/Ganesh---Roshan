47e2734115da52abc266d2e8183a9f6cb85708c6
Ganesh Kumar K <91590055+kganeshkumar11@users.noreply.github.com>
2023-03-18 17:47:12 +0530

    Update Qn 2.c

--- C Programming Training/Week 3/Qn 2.c
+++ C Programming Training/Week 3/Qn 2.c
@@ -1,1 +1,37 @@
- 
+#include<stdio.h>
+main() 
+{
+    int i,j,n,a;
+    printf("Enter the size of the matrix: ");
+    scanf("%d",&n);
+    int mat[n][n];
+    printf("Enter the elements of the matrix:\n");
+    for(i=0;i<n;i++) 
+    {
+        for(j=0;j<n;j++) 
+        {
+            scanf("%d",&mat[i][j]);
+        }
+    }
+    printf("Enter the element to be replaced: ");
+    scanf("%d",&a);
+    for(i =1;i<n;i++) 
+    {
+        for(j=0;j<n;j++) 
+        {
+            if(mat[i][j]==a) 
+            {
+                mat[i][j]=mat[i-1][j];
+            }
+        }
+    }
+    printf("Replaced matrix:\n");
+    for(i=0; i<n;i++) 
+    {
+        for(j=0; j<n;j++) 
+        {
+            printf("%d ",mat[i][j]);
+        }
+        printf("\n");
+    }
+}
